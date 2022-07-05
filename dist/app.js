d381d585b4ed0af74e874732dbef4fecb48ceaea
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-05 01:27:46 +0000

    Middleware and routes

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
+app.use("/todos", todos_1.default);
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000, () => console.log("Server running on port 3000"));
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
 import express from "express";
+import todoRoutes from "./routes/todos"
+
+
+
 const app = express()
+
+app.use("/todos", todoRoutes)
+
+
+// Error handling middleware
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+	res.status(500).json({message: err.message})
+})
+
 
 app.listen(3000, () => console.log("Server running on port 3000"))
